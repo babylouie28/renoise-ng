db7c69e5f442284a06a13040003b1e298e64d570
James Britt <james@neurogami.com>
2015-01-08 09:50:51 -0700

    Have timers using finctions from table

--- lua/com.neurogami.OscJumper.xrnx/OscJumper/Status.lua
+++ lua/com.neurogami.OscJumper.xrnx/OscJumper/Status.lua
@@ -3,6 +3,9 @@ Status = {}
 Status.current_pattern = 0 
 Status.is_polling = false
 
+Status.polls = {}
+
+  
 function Status.sequence_pos()
   --  local lines_passed = 0 --global buffer!
   --    local song = renoise.song()
@@ -14,11 +17,12 @@ function Status.sequence_pos()
 end
 
 
-function Status.status_poller()
+
+Status.polls["current_pattern_poll"] = function ()
   local OscMessage = renoise.Osc.Message
   local OscBundle = renoise.Osc.Bundle
   Status.sequence_pos()
-  print("Status.status_poller. Status.current_pattern = ", Status.current_pattern )
+  print(" Status.polls.current_pattern_poll = ", Status.current_pattern )
 
   CONTROLLER_OSC:send( OscMessage("/ng/current_pattern", { 
     {tag="i", value=Status.current_pattern} 
@@ -28,9 +32,9 @@ end
 
 function Status.stop_status_poller()
   print("\tStatus.stop_status_poller()")
-  if(renoise.tool():has_timer(Status.status_poller)) then
+  if(renoise.tool():has_timer(Status.polls["current_pattern_poll"])) then
     print("Remove the poller ...")
-    renoise.tool():remove_timer(Status.status_poller)
+    renoise.tool():remove_timer(Status.polls["current_pattern_poll"])
   end
   Status.is_polling = false
 end
@@ -41,7 +45,26 @@ function Status.start_status_poller(interval)
   Status.stop_status_poller()
   Status.is_polling = true
   print("\t* * * * * * Status.start_status_poller() * * * * * * " )
-  renoise.tool():add_timer(Status.status_poller, interval)
+  renoise.tool():add_timer(Status.polls["current_pattern_poll"], interval)
 end
+
+--[[
+
+The idea is to accept an OSC message that contains three things:
+* A status polling ID
+* A polling interval
+* The data to poll for, such as BPM, master volume, track N volume, etc.
+
+Data sent back would return with the polling ID so the client would know what it is.
+
+The ID is a string to make client code more readable. 
+
+
+Possibility #1: Pass Lua code to define the polling function.
+
+Thing is, `has_timer` expects a function reference, so the function needs to exist somehow.
+
+--]]
+
 
 
