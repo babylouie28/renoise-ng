40f47966ea35beda1a191c16312c9e49807a6001
James Britt <james@neurogami.com>
2015-01-14 11:43:14 -0700

    Fuxing up volume jumper GUI

--- lua/com.neurogami.OscJumper.xrnx/OscJumper/VolumeJumper.lua
+++ lua/com.neurogami.OscJumper.xrnx/OscJumper/VolumeJumper.lua
@@ -1,8 +1,15 @@
 -- Consider how to put this into a module or something
 
+volume_jumper_timers = {}
+volume_jumper_track_col_odds = {}
+volume_jumper_track_odds = {}
+volume_jumper_track_stop_odds = {}
 
 local view_voljumper_config_dialog 
 local configuration_dialog
+
+local SIMPLE_SPACE = 32
+local INPUT_FIELD_WIDTH = 32
 
 function configuration_dialog_keyhander(dialog, key)
   if key.name == "esc" then
@@ -12,17 +19,9 @@ function configuration_dialog_keyhander(dialog, key)
   end
 end
 
-volume_jumper_timers = {}
-volume_jumper_track_col_odds = {}
-volume_jumper_track_odds = {}
-volume_jumper_track_stop_odds = {}
-
-
-
 function reset_note_volumes(track_index)
   solo_note_column_volume(track_index, 1)
 end
-
 
 function solo_note_column_volume(track_index, note_column_index)
   local track = renoise.song().tracks[track_index]
@@ -46,13 +45,6 @@ function solo_note_column(track_index)
 
   print("solo_note_column(", track_index ,"). column_odds = ", column_odds )
 
-  -- Gen a rand number from 0 to 1
-  -- Iterate of the table of odds, keeping track of what
-  -- the index is.
-  -- When you hit a number that is > than the rand number
-  -- then use the corresponding index as the note column number.
-  -- Make that column unmute; mute the others.
-
   local r = math.random()
 
   -- FIXME Need to code the real function to 
@@ -60,14 +52,13 @@ function solo_note_column(track_index)
   -- The function needs to account for
   -- actual location: col 1 always the "no solo" value
   local col_to_solo = select_note_col(track_index)
+
   solo_note_column_volume(track_index, col_to_solo )
 
 end
 
 function volume_jumper_config() 
   print("volume_jumper_config() ")
-
-  -- Need something to hold values.
 
   local timer_interval = 1000
   local trigger_percentage = 10
@@ -76,8 +67,6 @@ function volume_jumper_config()
   local track = renoise.song().tracks[track_index]
   local note_cols_num = track.visible_note_columns
 
-
-  ---------
   local vb = renoise.ViewBuilder()
 
   view_voljumper_config_dialog = vb:column {
@@ -91,34 +80,47 @@ function volume_jumper_config()
         text = "ms for timer interval",
         tooltip = "ms for timer interval",
       },
+      vb:space { width = SIMPLE_SPACE },
       vb:textfield {
         text = ("" .. timer_interval),
         tooltip = "ms for timer interval",
+        width = INPUT_FIELD_WIDTH,
         notifier = function(v)
           timer_interval = tonumber(v)
         end
       },
+    },
+    vb:horizontal_aligner {
+      mode = "justify",
 
       vb:text {
         text = "Percentage for triggering (1 to 100)",
         tooltip = "Percentage for triggering (1 to 100)",
       },
+      vb:space { width = SIMPLE_SPACE },
       vb:textfield {
         text = ("" .. trigger_percentage),
         tooltip = "ms for timer interval",
+        width = INPUT_FIELD_WIDTH,
         notifier = function(v)
           trigger_percentage = tonumber(v)
         end
       },
 
+    },
+    vb:horizontal_aligner {
+      mode = "justify",
 
       vb:text {
         text = "Percentage for unsoloing  (1 to 100)",
         tooltip = "Percentage for unsoloing (1 to 100)",
       },
+
+      vb:space { width = SIMPLE_SPACE },
       vb:textfield {
         text = ("" .. solo_stop_percentage),
         tooltip =  "% for unsolo",
+        width = INPUT_FIELD_WIDTH,
         notifier = function(v)
           solo_stop_percentage = tonumber(v)
         end
@@ -130,6 +132,7 @@ function volume_jumper_config()
   } -- end of vb:colum
 
   local note_column_odds = {}
+
   for i = 2,note_cols_num  do
 
     local horiz_note_vol_form = vb:horizontal_aligner {
@@ -141,6 +144,7 @@ function volume_jumper_config()
       vb:textfield {
         text = "20",
         tooltip = "Odds of selection",
+        width = INPUT_FIELD_WIDTH,
         notifier = function(v)
           note_column_odds[i] = tonumber(v)
         end
@@ -153,20 +157,35 @@ function volume_jumper_config()
   end  -- end of note volume columns 
 
   local action_buttons = vb:horizontal_aligner {
-    mode = "justify",    
+    mode = "center",    
     vb:button {
       text = "Apply",
+      color = {227,255,150 },
       released = function()
         configuration_dialog:close()
         assign_vol_column_timers(timer_interval, trigger_percentage, track_index, note_column_odds, solo_stop_percentage )
       end
     },
-
+    vb:space {
+      width = SIMPLE_SPACE
+    },
     vb:button {
       text = "Clear existing timers",
+      color = {200, 255, 255 },
       released = function()
         configuration_dialog:close()
         clear_vol_column_timers(track_index)
+      end
+    },
+    vb:space{
+      width = SIMPLE_SPACE
+    },
+
+    vb:button {
+      text = "Cancel",
+      color = {255, 200, 200 },
+      released = function()
+        configuration_dialog:close()
       end
     },
   }
@@ -179,16 +198,12 @@ function select_note_col(track_index)
 end
 
 function clear_vol_column_timers(track_index)
-
   if(OscJumper.timers[track_index] and renoise.tool():has_timer( OscJumper.timers[track_index] ) ) then
     print("Remove the poller " , track_index, " ...")
     renoise.tool():remove_timer( OscJumper.timers[track_index] )
   end
   reset_note_volumes(track_index)
-
-
 end
-
 
 function assign_vol_column_timers(timer_interval, trigger_percentage, track_index, note_column_odds, solo_stop_percentage)
 
