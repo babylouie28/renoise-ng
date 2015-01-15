76d26074550f24b8f70497e186ae3f357a15957a
James Britt <james@neurogami.com>
2015-01-15 13:41:30 -0700

    readme

---
+++ lua/com.neurogami.RandyNoteColumns.xrnx/README.md
@@ -0,0 +1,29 @@
+# Randy Note Columns
+
+## Description
+
+## Usage
+
+
+
+## Author
+
+Randy Note Columns was written by James Britt / Neurogami.
+
+Source code can be found [here](https://github.com/Neurogami/renoise-ng/tree/master/lua/com.neurogami.RandyNoteColumns.xrnx).
+
+Send questions and comments to james@neurogami.com
+
+## Licence
+
+MIT License.
+
+
+Feed your head
+
+Hack your world
+
+Live curious
+
+
+
--- lua/com.neurogami.RandyNoteColumns.xrnx/RandyNoteColumns/Core.lua
+++ lua/com.neurogami.RandyNoteColumns.xrnx/RandyNoteColumns/Core.lua
@@ -4,7 +4,6 @@ require "RandyNoteColumns/Utils"
 
 RandyNoteColumns = {}
 
-
 RandyNoteColumns.timers = {}
 RandyNoteColumns.volume_jumper_timers = {}
 RandyNoteColumns.volume_jumper_track_col_odds = {}
@@ -12,8 +11,6 @@ RandyNoteColumns.volume_jumper_normalized_col_odds = {}
 RandyNoteColumns.volume_jumper_track_odds = {}
 RandyNoteColumns.volume_jumper_track_stop_odds = {}
 RandyNoteColumns.volume_jumper_track_timer_interval = {}
-
-
 
 function RandyNoteColumns.solo_note_column_volume(track_index, note_column_index)
   local track = renoise.song().tracks[track_index]
@@ -28,22 +25,15 @@ function RandyNoteColumns.solo_note_column_volume(track_index, note_column_index
   end
 end
 
-
 function RandyNoteColumns.solo_note_column(track_index)
-  --local track = renoise.song().tracks[track_index]
-  -- local note_cols_num = track.visible_note_columns
   local r = math.random()
   local col_to_solo = RandyNoteColumns.select_note_col(track_index)
   RandyNoteColumns.solo_note_column_volume(track_index, col_to_solo )
-
 end
-
 
 function RandyNoteColumns.normalize_volume_jumper_track_col_odds(track_index)
   local raw_column_odds = RandyNoteColumns.volume_jumper_track_col_odds[track_index]
   local normalized = {}
-
-  print("normalize_volume_jumper_track_col_odds")
 
   local sum = 0
   for k,v in pairs(raw_column_odds) do
@@ -54,7 +44,6 @@ function RandyNoteColumns.normalize_volume_jumper_track_col_odds(track_index)
     normalized[k] = v/sum
   end
 
-  rPrint()
   sum = 0
   for k,v in pairs(normalized ) do
     normalized [k] = normalized [k] + sum
@@ -63,41 +52,20 @@ function RandyNoteColumns.normalize_volume_jumper_track_col_odds(track_index)
 
   RandyNoteColumns.volume_jumper_normalized_col_odds[track_index]  = normalized 
 
-  print(" Should have now created volume_jumper_normalized_col_odds[track_index]  ")
-  rPrint(RandyNoteColumns.volume_jumper_normalized_col_odds[track_index]  )
 end
 
 function RandyNoteColumns.select_note_col(track_index)
-  -- The assumption is that the values have  already beeen normalized
-  -- so that they represent a series of values from 0.0 to 1.0.
-  -- This function needs to get a rand in that range and then see
-  -- where it best fits.
-  -- Suppose we have four  odds values:  0.05, 0.55, 0.70, and 1.0
-  -- The odds for any given columns is the size of different between it and
-  -- the previous number.
-  -- That is,  we have a 0.3 gap for the last col, but only 0.05 for the first,
-  -- and 0.5 for the second, and 0.15 for the third.
-  -- If the rand is, say 0.61, the code needs to look at each of these
-  -- "odds"  values and see if it is less than the number.
-  --  First hit means a win.
-  --
-  print("------------ select_note_col for ", track_index, " -------------")
-
-  print(" SELECT A COLUMN USING volume_jumper_normalized_col_odds[track_index] ")
-  rPrint(RandyNoteColumns.volume_jumper_normalized_col_odds[track_index])
-
+  
   local column_odds = RandyNoteColumns.volume_jumper_normalized_col_odds[track_index]
-  print("column_odds = ", column_odds )
   local r =  math.random()
 
   for col,v in pairs(column_odds) do
-    print("col", col, " = ", v )
     if (r < v) then
         return col
     end
   end
 
-  return 1 -- What's the best behavior if no other mtch comes up?
+  return 1 -- What's the best behavior if no other match comes up?
 end
 
 function RandyNoteColumns.reset_note_volumes(track_index)
@@ -106,7 +74,6 @@ end
 
 function RandyNoteColumns.clear_vol_column_timers(track_index)
   if(RandyNoteColumns.timers[track_index] and renoise.tool():has_timer( RandyNoteColumns.timers[track_index] ) ) then
-    print("Remove the poller " , track_index, " ...")
     renoise.tool():remove_timer( RandyNoteColumns.timers[track_index] )
   end
   RandyNoteColumns.reset_note_volumes(track_index)
@@ -115,8 +82,6 @@ end
 
 function RandyNoteColumns.assign_vol_column_timers(timer_interval, trigger_percentage, track_index, note_column_odds, solo_stop_percentage)
 
-  print("assign_vol_column_timers. note_column_odds = ", note_column_odds )
-  rPrint(note_column_odds)
 
   RandyNoteColumns.volume_jumper_track_timer_interval[track_index] = timer_interval
   RandyNoteColumns.volume_jumper_track_col_odds[track_index] = note_column_odds
@@ -125,9 +90,7 @@ function RandyNoteColumns.assign_vol_column_timers(timer_interval, trigger_perce
 
   RandyNoteColumns.normalize_volume_jumper_track_col_odds(track_index)
 
-
   local func_string = [[   
-  print("Multitimer for track ]] ..track_index .. [[ ")
   local track = renoise.song().tracks[]] .. track_index .. [[]
   local have_solo = track:column_is_muted(1)
   local rand_num = math.random(100)
@@ -135,34 +98,23 @@ function RandyNoteColumns.assign_vol_column_timers(timer_interval, trigger_perce
   local odds = RandyNoteColumns.volume_jumper_track_odds[]] .. track_index .. [[] 
   local stop_odds = RandyNoteColumns.volume_jumper_track_stop_odds[]] .. track_index .. [[]
   if (not have_solo) then
-    print("NO SOLO ...")
     if (odds > rand_num ) then
-      print("SET UP SWAP...")
       RandyNoteColumns.solo_note_column(]] .. track_index .. [[)
     end
   else
-    print("SOLO IS IN PLAY")
     odds = 50
     if (stop_odds > rand_num ) then
-      print("STOP THE SOLO")
       RandyNoteColumns.reset_note_volumes(]] .. track_index ..[[ )
     end
   end
   ]]
 
-
   if(RandyNoteColumns.timers[track_index] and renoise.tool():has_timer( RandyNoteColumns.timers[track_index] ) ) then
-    print("Remove the poller " ,track_index, " ...")
     renoise.tool():remove_timer( RandyNoteColumns.timers[track_index] )
   end
 
   -- Stuff can go wrong here, though there is no way at
   -- the moment to inform the client of that
   RandyNoteColumns.timers[track_index] = assert(loadstring(func_string))
-  print("Add the poller " , track_index , " ...")
   renoise.tool():add_timer(RandyNoteColumns.timers[track_index], timer_interval)
 end
-
-
-
-
