63c8c53f9d8eb868e9bdedfc1e8d5b04cfc7ca3e
James Britt <james@neurogami.com>
2015-01-22 23:48:18 -0700

    Have decent working version

--- lua/com.neurogami.PatternJumper.xrnx/PatternJumper/Core.lua
+++ lua/com.neurogami.PatternJumper.xrnx/PatternJumper/Core.lua
@@ -7,23 +7,52 @@ PatternJumper.current_loop_count = 0
 PatternJumper.current_loop = 1 
 
 PatternJumper.loop_list = {
-   {1, 2, 4},
-   {3, 4, 2 },
-   {1, 2, 2},
-   {5, 6, 2},
-   {3, 4, 4 },
+--  {1, 2, 1},
+  {5, 6, 1},
+  {3, 4, 1 },
 } 
 
 
+
+function are_we_in_this_loop(loop_index) 
+  local pos_start = song().transport.loop_start
+  local pos_end = song().transport.loop_end
+
+  local range_start = PatternJumper.loop_list[loop_index][1]
+  local range_end = PatternJumper.loop_list[loop_index][2]
+
+  if (pos_start  == range_start ) and (pos_end == range_end ) then
+    return true
+  else
+    return false
+  end
+
+end
+
 function set_next_loop()
   if PatternJumper.current_loop < #PatternJumper.loop_list then
-     print("There is a next loop")
+    print("There is a next loop. Current loop is ", PatternJumper.current_loop )
     PatternJumper.current_loop = PatternJumper.current_loop + 1
-     PatternJumper.loop_schedule(PatternJumper.loop_list[PatternJumper.current_loop][1], PatternJumper.loop_list[PatternJumper.current_loop][2])       
+
+    
+    print("Go get loop at index ", PatternJumper.current_loop );
+
+    local new_loop = PatternJumper.loop_list[PatternJumper.current_loop]
+
+
+    PatternJumper.current_loop_count = 0
+    PatternJumper.current_pattern = new_loop[1]
+    PatternJumper.last_pattern = new_loop[1]
+
+    PatternJumper.loop_schedule(new_loop[1], new_loop[2])       
   else
     print("There is no next loop. Now what?")
+    -- Clear all schedulated loops and let the song play out.
+     renoise.tool():remove_timer(process_looping)
+     PatternJumper.loop_clear()
   end
 end
+
 --[[
 
 
@@ -44,48 +73,93 @@ We need something to track that this transistion is going to occur so that the c
 tracking stuff isn't confused.
 
 
+Posible states:
+
+The start, in loop N 
+In loop N, and not at the last pattern
+In loop N, and at the last pattern, but not at the last loop count
+In loop N, and at the last pattern, and at the last loop count
+Just moved from loop N to N+1
+
+
 --]]
 function process_looping()
 
   local range_start, range_end, count, num_loops
 
+  range_start = PatternJumper.loop_list[PatternJumper.current_loop][1]
+  range_end = PatternJumper.loop_list[PatternJumper.current_loop][2]
+
+
   PatternJumper.current_pattern  = renoise.song().sequencer.pattern_sequence[renoise.song().transport.playback_pos.sequence]
+  
+  print("Current pattern: ", PatternJumper.current_pattern, "; loop count = ",  PatternJumper.current_loop_count )
+  
+  if (PatternJumper.current_pattern ~= PatternJumper.last_pattern ) then
+    if (PatternJumper.current_pattern == range_end ) then       
+      print("We are in the last patter of the loop.")
 
-  set_next_loop()
+      PatternJumper.current_loop_count = PatternJumper.current_loop_count + 1
+     if PatternJumper.current_loop_count  == PatternJumper.loop_list[PatternJumper.current_loop][3] then
+       print("* * * * * Loop count == max looping, so set next loop * * * * *")
+       set_next_loop()
+     else
+       ---- ???
+    end
+  end
 
-  for i, v in ipairs(PatternJumper.loop_list) do
 
-   rPrint(v)
-   range_start = v[1]
-   range_end = v[2]
-   num_loops = v[3]
- end
+  else
 
+  end
+
+  PatternJumper.last_pattern = PatternJumper.current_pattern 
+  
+end
+
+function PatternJumper.loop_clear()
+local song = renoise.song
+  print("PatternJumper.loop_clear(). Set next to PatternJumper.current_pattern ", PatternJumper.current_pattern)
+  -- PatternJumper.loop_schedule(PatternJumper.current_pattern , PatternJumper.current_pattern )
+  local pos_start = song().transport.loop_start
+  pos_start.line = 1; 
+
+  pos_start.sequence = PatternJumper.current_pattern
+
+  local pos_end = song().transport.loop_end
+  pos_end.line = 1; 
+
+  pos_end.sequence  = PatternJumper.current_pattern
+  song().transport.loop_range = {pos_start, pos_end}
+-- song().transport:set_scheduled_sequence( PatternJumper.current_pattern  )
 end
 
 
+
 function PatternJumper.loop_schedule(range_start, range_end)
-    local song = renoise.song
-    print("/loop/schedule! ", range_start, " ", range_end)
-    song().transport:set_scheduled_sequence(clamp_value(range_start, 1, song().transport.song_length.sequence))
-    local pos_start = song().transport.loop_start
-    pos_start.line = 1; pos_start.sequence = clamp_value(range_start, 1, song().transport.song_length.sequence)
-    local pos_end = song().transport.loop_end
-    pos_end.line = 1; pos_end.sequence =  clamp_value(range_end + 1, 1, 
-    song().transport.song_length.sequence + 1)
-    song().transport.loop_range = {pos_start, pos_end}
+  local song = renoise.song
+  print("/loop/schedule! ", range_start, " ", range_end)
+  song().transport:set_scheduled_sequence(clamp_value(range_start, 1, song().transport.song_length.sequence))
+  local pos_start = song().transport.loop_start
+  pos_start.line = 1; 
+  pos_start.sequence = clamp_value(range_start, 1, song().transport.song_length.sequence)
+  local pos_end = song().transport.loop_end
+  pos_end.line = 1; 
+  pos_end.sequence =  clamp_value(range_end + 1, 1, 
+  song().transport.song_length.sequence + 1)
+  song().transport.loop_range = {pos_start, pos_end}
 end
 
 
 
 function PatternJumper.go() 
-
-
-  local range_start, range_end, count, num_loops
-
+  
+  PatternJumper.current_pattern = 1 
+  PatternJumper.last_pattern = PatternJumper.current_pattern
+  
+  PatternJumper.current_loop_count = 0
+  PatternJumper.loop_schedule(PatternJumper.loop_list[PatternJumper.current_loop][1], PatternJumper.loop_list[PatternJumper.current_loop][2])
   renoise.tool():add_timer(process_looping, 1000)
-  
-  
 
 end
 
