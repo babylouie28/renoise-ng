1e7ab7eb031c5f20cca890a43fc6263f4b347619
James Britt <james@neurogami.com>
2015-07-19 12:58:41 -0700

    Have code to insert a gainer on master

--- lua/com.neurogami.MasterMuter.xrnx/MasterMuter/Core.lua
+++ lua/com.neurogami.MasterMuter.xrnx/MasterMuter/Core.lua
@@ -1,6 +1,7 @@
 -- Core.lua 
 MasterMuter = {}
 
+MasterMuter.mute_gainer_name = "MUTE_GAINER"
 
 
 function MasterMuter.get_master_track(song) 
@@ -14,9 +15,37 @@ function MasterMuter.get_master_track(song)
     return t
 end
 
+function MasterMuter.set_up_muter_gainer(track)
+  print( "MasterMuter.set_up_muter_gainer(", track, ")" )
+
+  -- rprint(track.available_devices)
+
+-- Get a list of devices. If we find a device with the name "master_mute_gainer"
+-- then return.
+-- If no such device exists, add a gain device and set the volume to -INF and disable it
+
+for index, device in ipairs(track.devices) do
+    if device.name == MasterMuter.mute_gainer_name then -- Skip TrackVolPan device
+       return device
+    end
+  end
+
+-- track.available_devices.
+-- local gainer = track:insert_device_at("Audio/Effects/Native/Gainer", 2) -- Doesn't like index 1
+   -- local gainer = track:insert_device_at("Audio/Effects/    Native/Gainer", 2) -- Doesn't like index 1
+   local gainer = track:insert_device_at("Audio/Effects/Native/Gainer", 2) -- Doesn't like index 1
+   print("gainer  = ", gainer)
+
+end
+
 function MasterMuter.manage_master_mute(mute_value)
-    local song = renoise.song
+   
+  print("MasterMuter.manage_master_mute(",mute_value,")" )
+
+  local song = renoise.song
     local master = MasterMuter.get_master_track(song) 
     print("master = ", master)
+
+    MasterMuter.set_up_muter_gainer(master)
 end
 
