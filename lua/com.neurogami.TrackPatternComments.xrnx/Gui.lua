e9da8dfe2eb2ec5f9e63c83bcc2bed8df466eafe
Neurogami <james@neurogami.com>
2017-11-02 21:45:52 -0700

    Added OK and cancel buttons

--- lua/com.neurogami.TrackPatternComments.xrnx/Gui.lua
+++ lua/com.neurogami.TrackPatternComments.xrnx/Gui.lua
@@ -9,39 +9,66 @@ GUI.guid = nil
 GUI.current_text = ""
 
 GUI.show_dialog = function()
-  
+
   if GUI.dialog and GUI.dialog.visible then
     GUI.dialog:show()
     return
   else
     GUI.dialog = {}
   end
-  
+
   GUI.vb = renoise.ViewBuilder()
   GUI.guid = TPC.find_nearest_commnent_guid()
   GUI.current_text = ""  
 
-  local title = "New Comment"
+  local title = "New TP Comment"
 
   if (GUI.guid ~= nil ) then
     title = "Comments for TP Comments " .. GUI.guid  .. " (" .. GUI.guid  .. ")"
   end
 
   local content = GUI.vb:column {
+
     margin = 3,
-    GUI.vb:multiline_textfield {
-      id = "text_field",
-      width = 300,
-      height = 200,
-      text = TPC.get_comments(GUI.guid ),
-      notifier = function()
+    GUI.vb:row {
+      GUI.vb:multiline_textfield {
+        id = "text_field",
+        width = 300,
+        height = 200,
+        text = TPC.get_comments(GUI.guid ),
+        notifier = function()
+          GUI.current_text = GUI.vb.views.text_field.text
+        end
+      }
+    },
+    GUI.vb:row{
+
+      GUI.vb:horizontal_aligner {
+        mode = "center",
+      GUI.vb:button { id = "OK",  height = 22, width = 23, 
+      text = "Save",
+      released = function()
         GUI.current_text = GUI.vb.views.text_field.text
+        GUI.dialog:close()
+      end
+    },
+
+
+    GUI.vb:space {width = 214},
+
+      GUI.vb:button { id = "CANCEL",  height = 22, width = 23, 
+      text = "Cancel",
+      released = function()
+        GUI.current_text = "",
+        GUI.dialog:close()
       end
     }
-  } 
-  
-  GUI.dialog = renoise.app():show_custom_dialog(title, content)  
-  
+  }
+}  
+} 
+
+GUI.dialog = renoise.app():show_custom_dialog(title, content)  
+
 end
 
 renoise.tool().app_idle_observable:add_notifier(function()
